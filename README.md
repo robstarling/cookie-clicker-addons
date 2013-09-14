9cfb1efdf83350dfefc89a0405f941c6afa17bd1
Rob Starling <github@robstarling.org>
2013-09-13 23:13:36 -0700

    add helper.js for basic advice.

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 cookie-clicker-addons
 =====================
 
-Things to enhance your Cookie Clicker experience
+Things to enhance your [Cookie Clicker](http://orteil.dashnet.org/cookieclicker/) experience.
+
+For very basic advice on what to buy, either add run this in your browser's javascript console:
+
+    (function(){var s=document.createElement("script");s.type="text/javascript";s.src="https://raw.github.com/robstarling/cookie-clicker-addons/master/helper.js";document.body.appendChild(s);})();
+
+or add a bookmark with this url:
+
+    javascript:(function(){var s=document.createElement("script");s.type="text/javascript";s.src="https://raw.github.com/robstarling/cookie-clicker-addons/master/helper.js";document.body.appendChild(s);})();
+
+and click on it when you're on Cookie Clicker to get new UI elements to help you.
---
+++ helper.js
@@ -0,0 +1,89 @@
+(function(undefined,window,document,logFn,G){
+
+  var helper = {
+
+    "p3sf": function(x) {
+      var s=Array('','k','M','G','T','P','E','Z','Y');
+      var commas=0;
+      while(x>1000){
+        //console.log(x);
+        commas+=1;x/=1000
+      };
+      //console.log("commas = "+commas);
+      x=""+x;
+      if(x.length>4){x=x.substring(0,4)};
+      if(x[x.length-1]=="."){x=x.substring(0,x.length-1)};
+      return x+s[commas];
+    },
+
+    "dur": function(sec) {
+      s="";
+      if(sec>=86400){var d=Math.floor(sec/86400); sec-=d*86400; s+=""+d+"day, "};
+      if(sec>=3600){var h=Math.floor(sec/3600); sec-=h*3600; s+=""+h+"hr, "};
+      if(sec>=60){var m=Math.floor(sec/60); sec-=m*60; s+=""+m+"min, "};
+      if(sec>0 || s==""){s+=""+sec+"sec"};
+      if(s[s.length-1]==" "){s=s.substring(0,s.length-2)};
+      return s;
+    },
+
+    "advise": function(printer) {
+      var p3sf = this.p3sf;
+      var dur = this.dur;
+      var log = (printer == undefined) ? logFn : printer;
+      log("Current: "+p3sf(G.cookies)+" +"+p3sf(G.cookiesPs)+"/s (+"+
+          p3sf(G.cookiesPs/60)+"/min, "+p3sf(G.cookiesPs/3600)+"/hr)");
+      log("");
+      var best=undefined;
+      for(var i=0; i < G.ObjectsById.length; i++) {
+        var o = G.ObjectsById[i];
+        var oCps = o.cps();
+        var opCps = o.price/oCps;
+        log(o.name+" ("+p3sf(o.amount*oCps)+"/s total) -> +"+p3sf(oCps)+"/s => "+
+            p3sf(opCps)+"/cps");
+        if(o.price<G.cookies) {
+          log(">     Can buy ~"+Math.floor(G.cookies/o.price)+" now for "+p3sf(o.price));
+          if(best==undefined || opCps < best.pCps) {
+            best = {pCps: opCps, o: o};
+          }
+        } else {
+          log(">     Could buy one in "+
+              dur(Math.ceil((o.price-G.cookies)/G.cookiesPs))+" for "+p3sf(o.price));
+        }
+      }
+      if(best!=undefined) {
+        log("");
+        log("Best option now: "+best.o.name);
+      }
+      log("");
+      for(var i=0; i < G.UpgradesInStore.length; i++) {
+        var u = G.UpgradesInStore[i];
+        var m = u.name+" (cost: "+p3sf(u.basePrice)+")";
+        if(u.basePrice<G.cookies) {
+          log(m + ", Can buy now.");
+        } else {
+          log(m + ", in "+ dur(Math.ceil((u.basePrice-G.cookies)/G.cookiesPs)));
+        }
+      }
+    },
+
+    "advicePopup": function() {
+      var out = "";
+      var printer = function(s) { out += "\n" + s; }
+      this.advise(printer);
+      alert(out);
+    }
+  };
+
+  window.Helper = helper;
+
+  var r=document.createElement("div");
+  r.setAttribute('id','rHelper');
+  r.innerHTML='<a href="javascript:Helper.advicePopup();false;"><img src="http://robstarling.org/photo/me_icon.gif"></a>';
+  r.style.position='absolute';
+  r.style.width=31;
+  r.style.height=38;
+  r.style.top='35px';
+  r.style.right='20px';
+  document.body.appendChild(r);
+
+})(undefined,window,document,function(s){console.log(s)},Game);
